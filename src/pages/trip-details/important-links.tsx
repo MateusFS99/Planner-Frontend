45d546b899b80f24c3855d97e09c5a0d54d9235b
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2024-07-15 11:48:11 -0300

    - Feat: Listagem de Links Importantes

--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,27 +1,49 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+interface Link {
+  id: string;
+  title: string;
+  url: string;
+}
 
 export function ImportantLinks() {
+  const { trip_id } = useParams();
+  const [importantLinks, setImportantLinks] = useState<Link[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`trips/${trip_id}/links`)
+      .then((response) => setImportantLinks(response.data.links));
+  }, [trip_id]);
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links Importantes</h2>
 
-      <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva aoba
-            </span>
-            <a
-              href="#"
-              className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-            >
-              link aoba
-            </a>
+      {importantLinks.map((link) => {
+        return (
+          <div key={link.id} className="space-y-5">
+            <div className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">
+                  {link.title}
+                </span>
+                <a
+                  href="#"
+                  className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="text-zinc-400 size-5 shrink-0" />
+            </div>
           </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
-      </div>
+        );
+      })}
 
       <Button variant="secondary" size="full">
         <Plus className="size-5" />
