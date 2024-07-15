afc53e164ec5ccdc8e20414dcf5af0af80b683d0
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2024-07-14 22:42:23 -0300

    - Feat: Integração Atividades

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,31 +1,66 @@
-/* eslint-disable no-constant-condition */
-import { CircleCheck, CircleDashed } from "lucide-react";
+import { CircleCheck } from "lucide-react";
+import { api } from "../../lib/axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+interface Activity {
+  date: string;
+  activities: {
+    id: string;
+    title: string;
+    occurs_at: string;
+  }[];
+}
 
 export function Activities() {
+  const { trip_id } = useParams();
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`trips/${trip_id}/activities`)
+      .then((response) => setActivities(response.data.activities));
+  }, [trip_id]);
+
   return (
     <div className="space-y-8">
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-          <span className="text-xs text-zinc-500">Sabado</span>
-        </div>
-
-        <div className="space-y-2.5">
-          <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-            {true ? (
-              <CircleCheck className="size-5 text-lime-300" />
+      {activities.map((category) => {
+        return (
+          <div key={category.date} className="space-y-2.5">
+            <div className="flex gap-2 items-baseline">
+              <span className="text-xl text-zinc-300 font-semibold">
+                Dia {format(category.date, "d")}
+              </span>
+              <span className="text-xs text-zinc-500">
+                {format(category.date, "EEEE", { locale: ptBR })}
+              </span>
+            </div>
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map((activity) => {
+                  return (
+                    <div key={activity.id} className="space-y-2.5">
+                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="text-zinc-400 text-sm ml-auto">
+                          {format(activity.occurs_at, "HH:mm")}h
+                        </span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
             ) : (
-              <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              <p className="text-zinc-500 text-sm">
+                Nenhuma atividade cadastrada nessa data.
+              </p>
             )}
-            <span className="text-zinc-100">atividade aoba</span>
-            <span className="text-zinc-400 text-sm ml-auto">00:00h</span>
           </div>
-        </div>
-
-        <p className="text-zinc-500 text-sm">
-          Nenhuma atividade cadastrada nessa data.
-        </p>
-      </div>
+        );
+      })}
     </div>
   );
 }
