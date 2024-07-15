cafdf3b17dbcf47d561f833b9e8052e02975dec1
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2024-07-15 12:03:53 -0300

    - Feat: Melhoria Componentização Atividades

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,9 +1,10 @@
-import { CircleCheck } from "lucide-react";
+import { CircleCheck, Plus } from "lucide-react";
 import { api } from "../../lib/axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { CreateActivityModal } from "./create-activity-modal";
 
 interface Activity {
   date: string;
@@ -17,6 +18,16 @@ interface Activity {
 export function Activities() {
   const { trip_id } = useParams();
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
+    useState(false);
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true);
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false);
+  }
 
   useEffect(() => {
     api
@@ -25,42 +36,64 @@ export function Activities() {
   }, [trip_id]);
 
   return (
-    <div className="space-y-8">
-      {activities.map((category) => {
-        return (
-          <div key={category.date} className="space-y-2.5">
-            <div className="flex gap-2 items-baseline">
-              <span className="text-xl text-zinc-300 font-semibold">
-                Dia {format(category.date, "d")}
-              </span>
-              <span className="text-xs text-zinc-500">
-                {format(category.date, "EEEE", { locale: ptBR })}
-              </span>
-            </div>
-            {category.activities.length > 0 ? (
-              <div>
-                {category.activities.map((activity) => {
-                  return (
-                    <div key={activity.id} className="space-y-2.5">
-                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                        <CircleCheck className="size-5 text-lime-300" />
-                        <span className="text-zinc-100">{activity.title}</span>
-                        <span className="text-zinc-400 text-sm ml-auto">
-                          {format(activity.occurs_at, "HH:mm")}h
-                        </span>
-                      </div>
-                    </div>
-                  );
-                })}
+    <div className="flex-1 space-y-6">
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-semibold">Atividades</h2>
+        <button
+          type="button"
+          onClick={openCreateActivityModal}
+          className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+        >
+          Cadastrar Atividade
+          <Plus className="size-5" />
+        </button>
+      </div>
+
+      <div className="space-y-8">
+        {activities.map((category) => {
+          return (
+            <div key={category.date} className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia {format(category.date, "d")}
+                </span>
+                <span className="text-xs text-zinc-500">
+                  {format(category.date, "EEEE", { locale: ptBR })}
+                </span>
               </div>
-            ) : (
-              <p className="text-zinc-500 text-sm">
-                Nenhuma atividade cadastrada nessa data.
-              </p>
-            )}
-          </div>
-        );
-      })}
+              {category.activities.length > 0 ? (
+                <div>
+                  {category.activities.map((activity) => {
+                    return (
+                      <div key={activity.id} className="space-y-2.5">
+                        <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                          <CircleCheck className="size-5 text-lime-300" />
+                          <span className="text-zinc-100">
+                            {activity.title}
+                          </span>
+                          <span className="text-zinc-400 text-sm ml-auto">
+                            {format(activity.occurs_at, "HH:mm")}h
+                          </span>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <p className="text-zinc-500 text-sm">
+                  Nenhuma atividade cadastrada nessa data.
+                </p>
+              )}
+            </div>
+          );
+        })}
+      </div>
+
+      {isCreateActivityModalOpen && (
+        <CreateActivityModal
+          closeCreateActivityModal={closeCreateActivityModal}
+        />
+      )}
     </div>
   );
 }
